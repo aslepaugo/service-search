d0b96f30403eec2e0b96b0fd48a6786298683d6d
Aliaksei Slepau <aslepaugo@aleksc-mac.home>
2020-11-30 07:30:19 +0100

    Added jsin module and deseralization

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-f = open("data-2897-2019-01-22.json", "r", encoding="CP1251")
-print(f.read())
+import json
+
+
+with open("data-2897-2019-01-22.json", "r", encoding="CP1251") as bar_file:
+    bar_data = json.load(bar_file)
+
+print(type(bar_data))
