108a13b97329403521c5afbbbe1d698eec6cd3b3
Aliaksei Slepau <aslepaugo@aleksc-mac.home>
2020-11-30 07:42:11 +0100

    Output full list of bars

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import json
 
 
 with open("data-2897-2019-01-22.json", "r", encoding="CP1251") as bar_file:
-    bar_data = json.load(bar_file)
+    bar_list = json.load(bar_file)
 
-print('Bar name {}'.format(bar_data[0]['Name']))
-print('Bar coordinate Latitude: {}'.format(bar_data[0]['geoData']['coordinates'][0]))
-print('Bar coordinate Longitude: {}'.format(bar_data[0]['geoData']['coordinates'][1]))
+for bar_data in bar_list:
+
+    bar_name = bar_data['Name']
+    bar_latitude = bar_data['geoData']['coordinates'][0]
+    bar_longitude = bar_data['geoData']['coordinates'][1]
+
+    print("{} {} {}".format(bar_name, bar_latitude, bar_longitude))
