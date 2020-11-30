62fe747df0d28f1ebdd2f7fee57c9b363cc23108
Aliaksei Slepau <aslepaugo@aleksc-mac.home>
2020-11-30 07:37:48 +0100

    Added geo data

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import json
 with open("data-2897-2019-01-22.json", "r", encoding="CP1251") as bar_file:
     bar_data = json.load(bar_file)
 
-print(bar_data[0]['Name'])
+print('Bar name {}'.format(bar_data[0]['Name']))
+print('Bar coordinate Latitude: {}'.format(bar_data[0]['geoData']['coordinates'][0]))
+print('Bar coordinate Longitude: {}'.format(bar_data[0]['geoData']['coordinates'][1]))
